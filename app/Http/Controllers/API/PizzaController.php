8be53fe9cc45d9f5bac4f3ca68605b7a8b460bf5
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-11 01:09:30 +0300

    Moved to API namespace and show() method implemented

---
+++ app/Http/Controllers/API/PizzaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Pizza;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+
+class PizzaController extends \App\Http\Controllers\Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Pizza::with(['sizes', 'toppings'])->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pizza  $pizza
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $r)
+    {
+        return Pizza::where('name', $r->route('Pizza'))
+            ->with(['sizes', 'toppings'])
+            ->get();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pizza  $pizza
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pizza $pizza)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pizza  $pizza
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pizza $pizza)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pizza  $pizza
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pizza $pizza)
+    {
+        //
+    }
+}
