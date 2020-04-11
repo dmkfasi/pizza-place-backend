7cfccbd232589cfd05386d95b97fe600eed4de62
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-12 01:14:06 +0300

    Replaced Faker with real and meaningful description

--- database/seeds/PizzasSeeder.php
+++ database/seeds/PizzasSeeder.php
@@ -13,79 +13,77 @@ class PizzasSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
-
         DB::table('pizzas')->insert(
             [
                 [
                     'name' => 'Margherita',
-                    'description' => $faker->realText(),
+                    'description' => 'Typical Neapolitan pizza, made with San Marzano tomatoes, mozzarella cheese, fresh basil, salt and extra-virgin olive oil. Traditionally, it is made with fior di latte (cow\'s milk mozzarella) rather than buffalo mozzarella.',
                     'pictureFilename' => 'm.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Prosciutto e Funghi',
-                    'description' => $faker->realText(),
+                    'description' => 'Topped with tomato sauce, mozzarella, thin slices of prosciutto cotto, and mushrooms. Some varieties can be topped with olives or served drizzled with olive oil.',
                     'pictureFilename' => 'pef.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Marinara',
-                    'description' => $faker->realText(),
+                    'description' => 'A Neapolitan pizza in Italian cuisine prepared with plain marinara sauce and seasoned with oregano and garlic. It is very similar to a Pizza Margherita, however it lacks the typical Mozzarella or other cheeses.',
                     'pictureFilename' => 'ma.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Quattro Stagioni',
-                    'description' => $faker->realText(),
+                    'description' => 'Prepared in four sections with diverse ingredients, with each section representing one season of the year.',
                     'pictureFilename' => 'qs.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Capricciosa',
-                    'description' => $faker->realText(),
+                    'description' => 'Is a style of pizza in Italian cuisine prepared with mozzarella cheese, Italian baked ham, mushroom, artichoke and tomato.',
                     'pictureFilename' => 'c.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Quattro Formaggi',
-                    'description' => $faker->realText(),
+                    'description' => 'Topped with a combination of four kinds of cheese, as the name suggests. Traditionally, the cheeses should be mozzarella and three other, local cheeses, depending on the region, such as Gorgonzola, Fontina, and Parmigiano-Reggiano.',
                     'pictureFilename' => 'qf.png',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Vegetariana',
-                    'description' => $faker->realText(),
+                    'description' => 'This Italian pizza variety is created to appeal to the vegetarian palate. Pizza vegetariana consists of a basic pizza dough that is smeared with tomato sauce and topped with mozzarella and a combination of fresh, seasonal vegetables, typically zucchini, eggplants, and peppers, which are almost always pre-cooked.',
                     'pictureFilename' => 'v.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Diavola',
-                    'description' => $faker->realText(),
+                    'description' => 'A variety of Italian pizza that is traditionally topped with tomato sauce, mozzarella cheese, spicy salami, and hot chili peppers. Black olives are optional and can be added for extra flavor.',
                     'pictureFilename' => 'd.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Boscaiola',
-                    'description' => $faker->realText(),
+                    'description' => 'Traditional Italian pizza variety that is topped with mozzarella, porcini or champignon mushrooms, tomatoes, cream, and sliced Italian sausage. It is occasionally prepared without tomatoes and can be enriched with a drizzle of olive oil, olives, or fresh parsley.',
                     'pictureFilename' => 'b.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ],
                 [
                     'name' => 'Frutti di Mare',
-                    'description' => $faker->realText(),
+                    'description' => 'Topped with seafood such as scampi, squid, and mussels. Traditionally, the pizza is served without any cheese, as the seafood is placed on top of the dough and tomato sauce instead. Pizza ai frutti di mare is popular throughout Italy and Mediterranean countries such as Croatia.',
                     'pictureFilename' => 'fdm.jpg',
                     'basePrice' => 9.99,
-                    'baseCurrency' => 'USD'
+                    'baseCurrency' => 'EUR'
                 ]
             ]
         );
